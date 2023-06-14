f437986c7a8d6deba07e2769389c92e882fbcaf0
2dengine <support@2dengine.com>
2023-06-14 09:45:24 +0300

    example update

--- example/main.lua
+++ example/main.lua
@@ -4,7 +4,8 @@ local proj = love.filesystem.getIdentity()
 local dest = sav.."/"..proj..".love"
 
 love.maker = require("maker.main")
-local build = love.maker.newBuild("lua", "txt", "png", "zip") -- include ONLY the selected formats
+love.maker.setExtensions("lua", "txt", "png", "zip") -- include ONLY the selected formats
+local build = love.maker.newBuild('C:/path/to/love/game/') -- love project directory (same as main.lua)
 build:ignore('/readme.txt') -- ignore specific files or folders
 build:ignoreMatch('^/.git') -- ignore based on pattern matching
 build:allow("/images/exception.jpg") -- whitelist a specific file
